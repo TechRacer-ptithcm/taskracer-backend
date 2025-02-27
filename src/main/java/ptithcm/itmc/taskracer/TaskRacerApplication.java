ea93a94f09f03f36761342da75316f967cd43640
VinhNguyenD23 <nguyenthanhvinh31305@gmail.com>
2025-02-27 16:40:08 +0700

    Initial Commit

---
+++ src/main/java/ptithcm/itmc/taskracer/TaskRacerApplication.java
@@ -0,0 +1,13 @@
+package ptithcm.itmc.taskracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRacerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskRacerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ptithcm/itmc/taskracer/TaskRacerApplicationTests.java
@@ -0,0 +1,13 @@
+package ptithcm.itmc.taskracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskRacerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
