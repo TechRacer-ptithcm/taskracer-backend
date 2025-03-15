6ed6b61aa5c5951b8ccbf7fbcb97dc4299d73ed3
VinhNguyenD23 <nguyenthanhvinh31305@gmail.com>
2025-03-15 22:15:17 +0700

    security: config cors

--- src/main/java/ptithcm/itmc/taskracer/config/SecurityConfig.java
+++ src/main/java/ptithcm/itmc/taskracer/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ptithcm.itmc.taskracer.config;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ptithcm.itmc.taskracer.filter.JwtFilter;
 
 @Configuration
@@ -26,5 +29,19 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
